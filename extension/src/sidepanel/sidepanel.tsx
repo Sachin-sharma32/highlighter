489d5296f521a3f79e0a4b7575d0969e2ecbb99b
sachin <sachin2sharma001@gmail.com>
2026-04-24 14:39:24 +0000

    Revamp extension side panel

--- extension/src/sidepanel/sidepanel.tsx
+++ extension/src/sidepanel/sidepanel.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import SidePanel from "./SidePanel";
+import SidePanel from "./SidePanelView";
 import "../popup/popup.css";
 
 createRoot(document.getElementById("root")!).render(
